b2f7105d4ee59dddd2e1c96691e709e35ee23fee
Neeraj Sisodiya <neerajsisodiya37@gmail.com>
2024-01-11 18:17:51 +0530

    application with app routing

---
+++ app/meals/page.js
@@ -0,0 +1,37 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals(){
+    const meals = await getMeals();
+    return <MealsGrid meals={meals}/>
+}
+
+export default function mealsPage() {
+    return (
+        <>
+            <header>
+                <h1>
+                    Delicious meals created {' '}
+                    <span className={classes}>by you</span>
+                </h1>
+                <p>
+                    Choose your favourite receipe and cook it yourself . It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href='meals/share'>
+                        Share your favourite receipe
+                    </Link>
+                </p>
+            </header>
+            <main>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals....</p>}>
+                    <Meals />
+                </Suspense>
+            </main>
+
+        </>
+    )
+}
